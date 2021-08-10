33f778290fde4a8b8ead602a4f1a5bd087de4122
J4nN4w <88735678+J4nN4w@users.noreply.github.com>
2021-08-10 21:52:03 +0200

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,12 @@
+import multiprocessing
+import shutil
+
+
+class test1:
+    print("Number of available CPU cores: ", multiprocessing.cpu_count())
+
+    total, used, free = shutil.disk_usage("/")
+
+    print("Total: %d GiB" % (total // (2**30)))
+    print("Used: %d GiB" % (used // (2**30)))
+    print("Total: %d GiB" % (free // (2**30)))
